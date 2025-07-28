c1cbd13e0d633a9b4ad7dbd7f9b844430b6efbe1
Luiscef <enriquefranco2003@gmail.com>
2025-07-28 05:25:00 +0000

    correccion por culpa de no usar el que estaba en el repositorio

--- src/Controllers/Checkout/Accept.php
+++ src/Controllers/Checkout/Accept.php
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Controllers\Checkout;
-
-use Controllers\PublicController;
-
-class Accept extends PublicController
-{
-    public function run(): void
-    {
-        $dataview = array();
-        $token = $_GET["token"] ?: "";
-        $session_token = $_SESSION["orderid"] ?: "";
-        if ($token !== "" && $token == $session_token) {
-            $result = \Utilities\Paypal\PayPalCapture::captureOrder($session_token);
-
-            $dataview["orderjson"] = json_encode($result, JSON_PRETTY_PRINT);
-        } else {
-            $dataview["orderjson"] = "No Order Available!!!";
-        }
-        \Views\Renderer::render("paypal/accept", $dataview);
-    }
-}
-=======
 <?php
 
 namespace Controllers\Checkout;
@@ -49,4 +24,3 @@ class Accept extends PublicController
         \Views\Renderer::render("paypal/accept", $dataview);
     }
 }
->>>>>>> origin/steven
